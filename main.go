a95db2206224f8a216bac34611df9194c188146b
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-04-17 19:59:59 +0200

    First endpoint

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "github.com/gofiber/fiber/v2"
+
 func main() {
-	println("Hello, World!")
+	app := fiber.New()
+
+	app.Get("/", handleDefault)
+
+	app.Listen(":5001")
+}
+
+func handleDefault(ctx *fiber.Ctx) error {
+	return ctx.JSON(map[string]string{"/": "default endpoint"})
 }
