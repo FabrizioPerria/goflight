9056024a853f118a1f737774a4e7f4ce5264c5bf
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-04-18 17:45:20 +0200

    Make api group

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
+	api := app.Group("/api")
 
-	app.Get("/", handleDefault)
+	api.Get("/greet", handleDefault)
 
 	app.Listen(":5001")
 }
 
 func handleDefault(ctx *fiber.Ctx) error {
-	return ctx.JSON(map[string]string{"/": "default endpoint"})
+	return ctx.JSON(map[string]string{"/greet": "yo!"})
 }
